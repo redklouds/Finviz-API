60465c7f6990e55cacdd19350b1f2e4774e8b1f3
Danny ly <dannyly199@gmail.com>
2017-03-21 04:15:18 -0700

    added support for scrapping finviz.com main page left column and right column data

--- finviz.py
+++ finviz.py
@@ -1,8 +1,11 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-def getMainPageData():
+def getMainColumnData(column):
         """
+        Parameters : Takes Left or Right
+            Left will be 0 right will be 1
+        
         This methid get's the two main left and right columns
         from finviz.com, the columns shown on the left are:
         Top Gainers
@@ -22,6 +25,60 @@ def getMainPageData():
         Earnings After
         Insider Selling
         """
+        #make the https requests
+        req = requests.get('http://finviz.com/')
+        #brew the soup >:)
+        soup  = BeautifulSoup(req.text,'html5lib')
+        #find the repsective elements we are looking for
+        #this will return a result set object for docs: look at website
+        #returns all matching elements
+        searchResult = soup.findAll('table',{'class':'t-home-table'})
+        # we just want the first and second matches
+        return searchResult[column]
+
+def displayLeftCol():
+        #returns a tag element object
+        data = getMainColumnData(0)
+        data = data.findChild()
+        data = list(data.children)
+        
+        for idx in data:
+                try:
+                        #print(item.getText())
+                        #populate the dictionary with respective data
+                        result = __parseText__(idx.getText())
+                        print("%s %s %s %s %s" %(result['index'],
+                                                 result['price'],
+                                                 result['change'],
+                                                 result['signal'],
+                                                 result['volume'])
+                              )
+                              
+                except:
+                        pass
+
+def displayRightCol():
+        #returns a tag element object
+        data = getMainColumnData(0)
+        data = data.findChild()
+        data = list(data.children)
+        
+        for idx in data:
+                try:
+                        #print(item.getText())
+                        #populate the dictionary with respective data
+                        result = __parseText__(idx.getText())
+                        print("%s %s %s %s %s" %(result['index'],
+                                                 result['price'],
+                                                 result['change'],
+                                                 result['signal'],
+                                                 result['volume'])
+                              )
+                              
+                except:
+                        pass
+
+        
 def getMainLeftColumn():
         q = requests.get('http://finviz.com/')
         #make the soup
@@ -55,10 +112,14 @@ def __parseText__(text):
         ##define regEx pattern
         #"find all alpha upper and lower words
         #+ one and unlimited timees
-        regExText = '[A-Za-z]+'
+        #match words who may or may not have spcaes betweent hem and
+        #are mixed caes zero to unliited times
+        regExText = '[A-Z a-z]+'
         #match a number with at least 1 to unlimited length
         ##the number must have a period and 1 through 2 numbers after it
-        regExDigit = '(\d+.\d{1,2})'
+        #match fully if there is a '+' OR '-' ZERO or 1 times
+        regExDigit = '(\+|-?\d+.\d{1,2})'
+        #regExDigit = '(\d+.\d{1,2})'
         listText = re.findall(regExText, text)
         listDigit = re.findall(regExDigit, text)
         resultSet = {'index':listText[0],
@@ -75,6 +136,6 @@ def test():
         print("hello world")
         getMainLeftColumn()
 
-
+test()
 if "__name__" == "__main__":
 	test()
